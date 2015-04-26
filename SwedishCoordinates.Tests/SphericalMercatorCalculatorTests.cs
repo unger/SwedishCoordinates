778ee1ba16aecbe72fe732234494d510d8ca2c46
Magnus Unger <magnus_unger@hotmail.com>
2015-04-26 09:48:07 +0200

    Conversion between plan coordinates and latitude/longitude

---
+++ SwedishCoordinates.Tests/SphericalMercatorCalculatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwedishCoordinates.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SphericalMercatorCalculatorTests
+    {
+        /*
+            Välen
+            7890733,1325761
+            6396528,1267363
+            57.633573,11.909510         
+         */
+
+        [TestCase(1325761.0, 11.909510)]
+        public void XToLongitude(double x, double expectedLng)
+        {
+            var lng = new SphericalMercatorCalculator().XToLongitude(x);
+
+            Assert.Less(Math.Abs(lng - expectedLng), 0.00001);
+        }
+
+        [TestCase(11.909510, 1325761.0)]
+        public void LongitudeToX(double lng, double expectedX)
+        {
+            var x = new SphericalMercatorCalculator().LongitudeToX(lng);
+
+            Assert.Less(Math.Abs(x - expectedX), 0.5);
+        }
+
+        [TestCase(7890733.0, 57.633573)]
+        public void YToLatitude(double y, double expectedLat)
+        {
+            var lat = new SphericalMercatorCalculator().YToLatitude(y);
+
+            Assert.Less(Math.Abs(lat - expectedLat), 0.00001);
+        }
+
+        [TestCase(57.633573, 7890733.0)]
+        public void LatitudeToY(double lat, double expectedY)
+        {
+            var y = new SphericalMercatorCalculator().LatitudeToY(lat);
+
+            Assert.Less(Math.Abs(y - expectedY), 0.4);
+        }
+
+        [TestCase(57.633573)]
+        [TestCase(11.909510)]
+        public void LatitudeToY_YToLatitude(double lat)
+        {
+            var calc = new SphericalMercatorCalculator();
+            var newLat = calc.YToLatitude(calc.LatitudeToY(lat));
+
+            Assert.Less(Math.Abs(lat - newLat), 0.0000000000001);
+        }
+
+        [TestCase(7890733.0)]
+        [TestCase(1325761.0)]
+        public void YToLatitude_LatitudeToY(double y)
+        {
+            var calc = new SphericalMercatorCalculator();
+            var newY = calc.LatitudeToY(calc.YToLatitude(y));
+
+            Assert.Less(Math.Abs(y - newY), 0.0000001);
+        }
+    }
+}
---
+++ SwedishCoordinates/SphericalMercatorCalculator.cs
@@ -0,0 +1,75 @@
+﻿namespace SwedishCoordinates
+{
+    using System;
+
+    public class SphericalMercatorCalculator
+    {
+        private const double EarthRadius = 6378137.0;
+
+        // double y2lat_m(double y) { return rad2deg(2 * atan(exp((y / earth_radius))) - M_PI / 2); }
+        // function y2lat($y) { return rad2deg(2.0 * atan(exp($y / 6378137.0)) - M_PI_2); }
+        public double YToLatitude(double y)
+        {
+            return this.RadiansToDegrees(
+                (2 * Math.Atan(
+                    Math.Exp(y / EarthRadius))) - (Math.PI / 2));
+        }
+
+        // double lat2y_m(double lat) { return earth_radius * log(tan(M_PI / 4 + deg2rad(lat) / 2)); }
+        // function lat2y($lat) { return log(tan(M_PI_4 + deg2rad($lat) / 2.0)) * 6378137.0; }
+        public double LatitudeToY(double latitude)
+        {
+            return EarthRadius * (
+                Math.Log(
+                    Math.Tan(
+                        (Math.PI / 4.0) + (this.DegreesToRadians(latitude) / 2))));
+        }
+
+        // double x2lon_m(double x) { return rad2deg(x / earth_radius); }
+        // function x2lon($x) { return rad2deg($x / 6378137.0); }
+        public double XToLongitude(double x)
+        {
+            return this.RadiansToDegrees(x / EarthRadius);
+        }
+
+        // double lon2x_m(double lon) { return deg2rad(lon) * earth_radius; }
+        // function function lon2x($lon) { return deg2rad($lon) * 6378137.0; }
+        public double LongitudeToX(double longitude)
+        {
+            return this.DegreesToRadians(longitude) * 6378137.0;
+        }
+
+        /*
+        // double y2lat_m(double y) { return rad2deg(2 * atan(exp((y / earth_radius))) - M_PI / 2); }
+        // function y2lat($y) { return rad2deg(2.0 * atan(exp($y / 6378137.0)) - M_PI_2); }
+        public double YToLatitude(double y)
+        {
+            return this.RadiansToDegrees(2 * Math.Atan(Math.Exp(this.DegreesToRadians(y / EarthRadius)))) - (Math.PI / 2);
+            return this.RadiansToDegrees(2 * Math.Atan(Math.Exp(y / EarthRadius))) - (Math.PI / 2);
+        }
+
+        // double lat2y_m(double lat) { return earth_radius * log(tan(M_PI / 4 + deg2rad(lat) / 2)); }
+        // function lat2y($lat) { return log(tan(M_PI_4 + deg2rad($lat) / 2.0)) * 6378137.0; }
+        public double LatitudeToY(double latitude)
+        {
+            return this.RadiansToDegrees(
+                Math.Log(
+                    Math.Tan(
+                        (Math.PI / 4.0) + (this.DegreesToRadians(latitude) / 2))));
+            return EarthRadius *
+                Math.Log(
+                    Math.Tan(
+                        (Math.PI / 4.0) + (this.DegreesToRadians(latitude) / 2)));
+        }*/
+
+        private double DegreesToRadians(double degrees)
+        {
+            return degrees * (Math.PI / 180.0);
+        }
+
+        private double RadiansToDegrees(double radians)
+        {
+            return radians * (180.0 / Math.PI);
+        }
+    }
+}
