5ba7d821c0a2b22fafbbf77091ba2bda87828c34
Magnus Unger <magnus.unger@layer10.se>
2015-12-11 12:43:29 +0100

    Changed class library to portable with profile 78

--- SwedishCoordinates/Properties/AssemblyInfo.cs
+++ SwedishCoordinates/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dc4148af-d5ee-4130-b6f2-caa979e24f01")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
